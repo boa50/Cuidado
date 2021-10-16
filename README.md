ce4999a0360452bc7e66fa1f2e37c151dd2c7e65
boa50 <bruno.albuquerque50@gmail.com>
2021-10-16 18:12:26 -0300

    Melhorias na documentação do projeto

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Cuidado
+Para fazer as alterações no código e o deploy para o Arduino é preciso ter instalado o Arduino IDE no computador: https://www.arduino.cc/en/Guide/Linux
+
+O arquivo principal para execução é o TesteModuloSom.ino
+
+Para o controle do áudio e da recepção dos sinais dos sensores foi utilizado o módulo [DFPlayer Mini](https://www.arduinoecia.com.br/modulo-mp3-dfplayer-mini-dfrobot-arduino/)
+
+Para a leitura do movimento foi usado o sensor [hw-416-b](https://www.taydaelectronics.com/datasheets/files/A-4432.pdf)
--- TesteModuloSom/TesteModuloSom.ino
+++ TesteModuloSom/TesteModuloSom.ino
@@ -9,21 +9,24 @@ int sensor1 = 9;
 int sensor2 = 10;
 int sensor3 = 11;
 
+//Para visualizar os prints corretamente é preciso alterar o valor do baud para 19200
+//no Serial Monitor (Ctrl + Shift + M)
 void setup()
 {
-
+  Serial.print("Início do Setup\n");
+  
   mySoftwareSerial.begin(9600);
   Serial.begin(19200);
   delay(5000);
   
-  Serial.print("inicializa");
+  Serial.print("Inicialização dos Módulos\n");
   
   if (!myDFPlayer.begin(mySoftwareSerial))
   {
     while (true);
   }
 
-  Serial.print("inicializado");
+  Serial.print("Inicializado\n");
   myDFPlayer.volume(14);
   myDFPlayer.EQ(0);
   myDFPlayer.play(-47);
@@ -38,15 +41,15 @@ void loop()
   myDFPlayer.volume(14); 
   
   if(digitalRead(sensor1)){
-    Serial.print("sensor1");
+    Serial.print("sensor1\n");
     toca_alarme();
   }
   if(digitalRead(sensor2)){
-    Serial.print("sensor2");
+    Serial.print("sensor2\n");
     toca_alarme();
   }
   if(digitalRead(sensor3)){
-    Serial.print("sensor3");
+    Serial.print("sensor3\n");
     toca_alarme();
   }
 
